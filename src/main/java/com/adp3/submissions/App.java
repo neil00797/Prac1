a498ca6630f9514a78d99a32eda4a28ae5f8aaea
unknown <4romgod@gmail.com>
2020-02-17 16:33:17 +0200

    First commit

---
+++ src/main/java/com/adp3/submissions/App.java
@@ -0,0 +1,38 @@
+package com.adp3.submissions;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        Random randGen = new Random();
+
+        int randomNum = randGen.nextInt(10);
+
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        int guesses = 1;
+
+        while(number != randomNum){
+            if(number<randomNum){
+                System.out.print("Too Low, Try again: ");
+            }
+            else{
+                System.out.print("Too High, Try again: ");
+            }
+
+            number = input.nextInt();
+            guesses++;
+
+        }       //end while()
+
+        System.out.printf("\nYEY!... You got it right after %d tries\n", guesses);
+
+    }       //end main()
+
+}       //end class
